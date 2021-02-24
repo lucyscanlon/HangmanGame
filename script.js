bf1a8a4c3d8e596600116be9cbaeeb55a48f38f9
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2021-02-24 23:25:49 +0000

    new game functionality working

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ const livesUsedEl = document.querySelector('.lives--used');
 const winnerOverlay = document.querySelector('.winner--overlay--message');
 const loserOverlay = document.querySelector('.loser--overlay--message');
 const newGameEl = document.querySelector('.newgame--button');
+const gameTitle = document.querySelector('.maingame--title');
+const buttonsCssOff = document.querySelectorAll('.maingame-alphabet-div-centeralign--active');
+
+
+
 
 // secret word
 let secretWord = "HELLO";
@@ -22,6 +27,8 @@ let livesLeft = 10;
 let livesUsed = 0;
 let playing = true;
 let highScore = 0;
+
+let needsEventListenerArray = [];
 
 //setting all scores
 highScoreEl.textContent = 0;
@@ -35,100 +42,126 @@ livesUsedEl.textContent = 0;
 
 displayWord.textContent = "_____";
 
-// add event listeners to all alphabet buttons
-/*alphabetButtons.forEach(item =>{
-  item.addEventListener('click', function checkGuess() {
-    if(playing) {
-      const guessedLetter = item.textContent;
-      let letterCorrect = false;
+const makeClickable = function(element) {
+  element.addEventListener('click', function checkGuess() {
+    console.log('clicked');
 
-      // loops over secret word, if guess letter
-      // the same then replace that letter in the
-      // guessingword array.
-      // array joined and displayed to reveal
-      // correct guess letter to player
-      for(let i = 0; i < secretWord.length; i++) {
-        if(guessedLetter === secretWord[i]) {
-          guessingWord[i] = guessedLetter;
-          console.log(guessedLetter, 'correct');
-          displayWord.textContent = guessingWord.join('');
-          letterCorrect = true;
+    const guessedLetter = element.textContent;
+    let letterCorrect = false;
 
-          //update scores
-          lettersToGuess--;
-          lettersLeftEl.textContent = lettersToGuess;
+    for(let i = 0; i < secretWord.length; i++ ) {
+      if(guessedLetter === secretWord[i]) {
+        guessingWord[i] = guessedLetter;
+        console.log(guessedLetter, 'correct');
+        displayWord.textContent = guessingWord.join('');
+        letterCorrect = true;
 
+        //update scores
+        lettersToGuess--;
+        lettersLeftEl.textContent = lettersToGuess;
 
-          // Turning the correct selected letter green
-          // Removing event listener
-          item.parentNode.classList.add('button--correct--inactive');
-          item.removeEventListener('click', checkGuess);
-          item.classList.add('event--off');
-          }
-        }
-
-        // if letter correct is false then change
-        //color to red and remove event listener
-        if(!letterCorrect) {
-          console.log(guessedLetter, 'incorrect');
-          item.parentNode.classList.add('button--incorrect--inactive');
-          item.removeEventListener('click', checkGuess);
-          item.classList.add('event--off');
-
-          //update scores
-          score--;
-          livesLeft--;
-          livesUsed++;
-          currentScoreEl.textContent = score;
-          livesLeftEl.textContent = livesLeft;
-          livesUsedEl.textContent = livesUsed;
-        }
-
-        // when letters left guessing for reaches 0 display
-        // winner overlay and if lives have run out display
-        // loser overlay
-        if(lettersToGuess < 1) {
-          winnerOverlay.classList.remove('hidden');
-          playing = false;
-
-          if (score > highScore) {
-            highScore = score;
-            console.log(highScore);
-            highScoreEl.textContent = highScore;
-          }
-
-        }
-
-        if(livesLeft < 1) {
-          loserOverlay.classList.remove('hidden');
-          playing = false;
-        }
+        // Turning the correct selected letter green
+        // Removing event listener
+        element.parentNode.classList.add('button--correct--inactive');
+        element.removeEventListener('click', checkGuess);
+        element.classList.add('event--off');
+      }
     }
 
-    })
-  })
+    // if letter correct is false then change
+    //color to red and remove event listener
+    if(!letterCorrect) {
+      console.log(guessedLetter, 'incorrect');
+      element.parentNode.classList.add('button--incorrect--inactive');
+      element.removeEventListener('click', checkGuess);
+      element.classList.add('event--off');
 
-  // new game button clicked
+      //update scores
+      score--;
+      livesLeft--;
+      livesUsed++;
+      currentScoreEl.textContent = score;
+      livesLeftEl.textContent = livesLeft;
+      livesUsedEl.textContent = livesUsed;
 
-  newGameEl.addEventListener('click', function () {
-    secretWord = "HELLO";
-    guessingWord = ['_', '_', '_', '_', '_'];
-    score = 10;
-    lettersToGuess = 5;
-    livesLeft = 10;
-    livesUsed = 0;
-    playing = true;
-    highScore = 0;
+    }
 
-    loserOverlay.classList.add('hidden');
-    winnerOverlay.classList.add('hidden');
+    // when letters left guessing for reaches 0 display
+    // winner overlay and if lives have run out display
+    // loser overlay
+    if(lettersToGuess < 1) {
+      winnerOverlay.classList.remove('hidden');
+      playing = false;
 
-    alphabetButtons.forEach(item => {
-      if(item.classList.contains('event--off')){
-        item.addEventListener('click', checkGuess() );
+      if (score > highScore) {
+        highScore = score;
+        console.log(highScore);
+        highScoreEl.textContent = highScore;
       }
 
-    })
+    }
+
+    if(livesLeft < 1) {
+      loserOverlay.classList.remove('hidden');
+      playing = false;
+    }
+
+  })
+}
 
 
-  })*/
+//reset buttons css function
+const buttonStyleReset = function(element) {
+  element.classList.remove('button--correct--inactive');
+  element.classList.remove('button--incorrect--inactive');
+}
+
+
+//add event listeners to all alphabet keys at the start of the game
+alphabetButtons.forEach(item => {
+  if(playing) {
+    makeClickable(item);
+  }
+})
+
+//if the new game button is pressed
+newGameEl.addEventListener('click', function() {
+  console.log('hello');
+
+  //reset all scores
+  secretWord = "HELLO";
+  guessingWord = ['_', '_', '_', '_', '_'];
+  score = 10;
+  lettersToGuess = 5;
+  livesLeft = 10;
+  livesUsed = 0;
+  playing = true;
+  highScore = 0;
+
+  //updating the text content
+  currentScoreEl.textContent = 10;
+  lettersLeftEl.textContent = 5;
+  livesLeftEl.textContent = 10;
+  livesUsedEl.textContent = 0;
+
+  displayWord.textContent = "_____";
+
+
+  //hide winner and loser screen
+  loserOverlay.classList.add('hidden');
+  winnerOverlay.classList.add('hidden');
+
+  //call function to reset button css style
+  buttonsCssOff.forEach(item => {
+    buttonStyleReset(item);
+  })
+
+  //Add event listeners back to inactive buttons
+  const buttonsEventOff = document.querySelectorAll('.event--off');
+
+  buttonsEventOff.forEach(item => {
+    item.classList.remove('event--off')
+    makeClickable(item);
+  })
+
+})
