1f39bebda30856e1971d0775acd3730549361a35
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2021-02-25 00:13:19 +0000

    game fully functional

--- script.js
+++ script.js
@@ -14,12 +14,89 @@ const loserOverlay = document.querySelector('.loser--overlay--message');
 const newGameEl = document.querySelector('.newgame--button');
 const gameTitle = document.querySelector('.maingame--title');
 const buttonsCssOff = document.querySelectorAll('.maingame-alphabet-div-centeralign--active');
+const hangmanImgEl = document.querySelector('.hangman--img');
+const winnerOverlaySecretWord = document.querySelector('.winner--secretword');
+const loserOverlaySecretWord = document.querySelector('.loser--secretword');
 
+// generate random word function
 
+let randomWord = '';
 
+const generateWord = function() {
+  let randomNumber = Math.trunc(Math.random() * 20 + 1 );
+
+  switch(randomNumber) {
+    case 1:
+      randomWord = 'JAZZY';
+      break;
+    case 2:
+      randomWord = 'BOARD';
+      break;
+    case 3:
+      randomWord = 'BORED';
+      break;
+    case 4:
+      randomWord = 'CADET';
+      break;
+    case 5:
+      randomWord = 'CAROL';
+      break;
+    case 6:
+      randomWord = 'AGONY';
+      break;
+    case 7:
+      randomWord = 'EARLY';
+      break;
+    case 8:
+      randomWord = 'FABLE';
+      break;
+    case 9:
+      randomWord = 'HABIT';
+      break;
+    case 10:
+      randomWord = 'LABOR';
+      break;
+    case 11:
+      randomWord = 'MACHO';
+      break;
+    case 12:
+      randomWord = 'OASIS';
+      break;
+    case 13:
+      randomWord = 'SOBER';
+      break;
+    case 14:
+      randomWord = 'TABLE';
+      break;
+    case 15:
+      randomWord = 'VAGUE';
+      break;
+    case 16:
+      randomWord = 'YACHT';
+      break;
+    case 17:
+      randomWord = 'WACKY';
+      break;
+    case 18:
+      randomWord = 'UDDER';
+      break;
+    case 19:
+      randomWord = 'TABOO';
+      break;
+    case 20:
+      randomWord = 'SMILE';
+      break;
+
+  }
+
+  console.log(randomWord);
+
+}
+
+generateWord()
 
 // secret word
-let secretWord = "HELLO";
+let secretWord = randomWord;
 let guessingWord = ['_', '_', '_', '_', '_'];
 let score = 10;
 let lettersToGuess = 5;
@@ -27,6 +104,7 @@ let livesLeft = 10;
 let livesUsed = 0;
 let playing = true;
 let highScore = 0;
+let numberofTurn = 0;
 
 let needsEventListenerArray = [];
 
@@ -37,14 +115,14 @@ lettersLeftEl.textContent = 5;
 livesLeftEl.textContent = 10;
 livesUsedEl.textContent = 0;
 
-//check guess function
-
 
 displayWord.textContent = "_____";
 
+
+
+
 const makeClickable = function(element) {
   element.addEventListener('click', function checkGuess() {
-    console.log('clicked');
 
     const guessedLetter = element.textContent;
     let letterCorrect = false;
@@ -52,7 +130,6 @@ const makeClickable = function(element) {
     for(let i = 0; i < secretWord.length; i++ ) {
       if(guessedLetter === secretWord[i]) {
         guessingWord[i] = guessedLetter;
-        console.log(guessedLetter, 'correct');
         displayWord.textContent = guessingWord.join('');
         letterCorrect = true;
 
@@ -71,7 +148,6 @@ const makeClickable = function(element) {
     // if letter correct is false then change
     //color to red and remove event listener
     if(!letterCorrect) {
-      console.log(guessedLetter, 'incorrect');
       element.parentNode.classList.add('button--incorrect--inactive');
       element.removeEventListener('click', checkGuess);
       element.classList.add('event--off');
@@ -80,9 +156,13 @@ const makeClickable = function(element) {
       score--;
       livesLeft--;
       livesUsed++;
+      numberofTurn++;
       currentScoreEl.textContent = score;
       livesLeftEl.textContent = livesLeft;
       livesUsedEl.textContent = livesUsed;
+
+      // Update hangman image
+      hangmanImgEl.src = `img/hangman${numberofTurn}.PNG`;
 
     }
 
@@ -91,11 +171,11 @@ const makeClickable = function(element) {
     // loser overlay
     if(lettersToGuess < 1) {
       winnerOverlay.classList.remove('hidden');
+      winnerOverlaySecretWord.textContent = randomWord;
       playing = false;
 
       if (score > highScore) {
         highScore = score;
-        console.log(highScore);
         highScoreEl.textContent = highScore;
       }
 
@@ -103,6 +183,7 @@ const makeClickable = function(element) {
 
     if(livesLeft < 1) {
       loserOverlay.classList.remove('hidden');
+      loserOverlaySecretWord.textContent = randomWord;
       playing = false;
     }
 
@@ -126,10 +207,11 @@ alphabetButtons.forEach(item => {
 
 //if the new game button is pressed
 newGameEl.addEventListener('click', function() {
-  console.log('hello');
+
+  generateWord()
 
   //reset all scores
-  secretWord = "HELLO";
+  secretWord = randomWord;
   guessingWord = ['_', '_', '_', '_', '_'];
   score = 10;
   lettersToGuess = 5;
@@ -137,14 +219,18 @@ newGameEl.addEventListener('click', function() {
   livesUsed = 0;
   playing = true;
   highScore = 0;
+  numberofTurn = 0;
 
-  //updating the text content
+  //resetting the text content
   currentScoreEl.textContent = 10;
   lettersLeftEl.textContent = 5;
   livesLeftEl.textContent = 10;
   livesUsedEl.textContent = 0;
 
   displayWord.textContent = "_____";
+
+  // reset hangman image
+  hangmanImgEl.src = `img/hangman0.PNG`;
 
 
   //hide winner and loser screen
